de7f30a286cdc2bf1feb82b62a0837478040d8f8
Tom Sherman <the.tomsherman@gmail.com>
2017-08-15 21:40:07 +0100

    Rudimentary buffer size implementation

--- lib/send_file_with_range/controller_extension.rb
+++ lib/send_file_with_range/controller_extension.rb
@@ -6,19 +6,25 @@ module SendFileWithRange
       else
         super path, options
       end
+      debugger
     end
 
     def send_file_with_range(path, options = {})
       if File.exist?(path)
+        buffer_size = 100000
         file_size = File.size(path)
         begin_point = 0
         end_point = file_size - 1
+
         status = 200
         if request.headers['range']
           status = 206
           if request.headers['range'] =~ /bytes\=(\d+)\-(\d*)/
+            logger.info("Range: #{request.headers['range']}")
             begin_point = Regexp.last_match(1).to_i
             end_point = Regexp.last_match(2).to_i if Regexp.last_match(2).present?
+
+            end_point = begin_point + buffer_size if begin_point + buffer_size <= file_size - 1
           end
         end
         content_length = end_point - begin_point + 1
