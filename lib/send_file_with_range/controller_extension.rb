e2d46aa6e6ac37b581f3826364733c0c3c2d955b
Tom Sherman <the.tomsherman@gmail.com>
2017-08-15 21:17:14 +0100

    Use modern ruby syntax

--- lib/send_file_with_range/controller_extension.rb
+++ lib/send_file_with_range/controller_extension.rb
@@ -1,6 +1,5 @@
 module SendFileWithRange
   module ControllerExtension
-
     def send_file(path, options = {})
       if options[:range]
         send_file_with_range(path, options)
@@ -18,19 +17,18 @@ module SendFileWithRange
         if request.headers['range']
           status = 206
           if request.headers['range'] =~ /bytes\=(\d+)\-(\d*)/
-            begin_point = $1.to_i
-            end_point = $2.to_i if $2.present?
+            begin_point = Regexp.last_match(1).to_i
+            end_point = Regexp.last_match(2).to_i if Regexp.last_match(2).present?
           end
         end
         content_length = end_point - begin_point + 1
         response.header['Content-Range'] = "bytes #{begin_point}-#{end_point}/#{file_size}"
         response.header['Content-Length'] = content_length.to_s
         response.header['Accept-Ranges'] = 'bytes'
-        send_data IO.binread(path, content_length, begin_point), options.merge(:status => status)
+        send_data IO.binread(path, content_length, begin_point), options.merge(status: status)
       else
         raise ActionController::MissingFile, "Cannot read file #{path}"
       end
     end
-
   end
 end
